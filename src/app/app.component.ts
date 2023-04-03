5185347c57e7259738e1cb49de095ca6337e3e97
Kalyan <pavankalyannagulla28@gmail.com>
2023-04-03 22:43:36 +0530

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'emi';
+
+  public P: number = 10000;
+  public r:number = 10.25;
+  public n:number = 1;
+  public EMI: number =0;
+
+  public Amountchange(e:any){
+    this.P = e.target.value;
+  }
+
+  public Yearchange(e:any){
+    this.n = e.target.value;
+  }
+
+  public Ratechange(e:any){
+    this.r = e.target.value;
+  }
+
+  public CalculateClick(){
+    var n = this.n * 12;
+    var r = this.r / 12 / 100;
+    this.EMI = this.P * r * Math.pow(1 + r, n) / Math.pow(1+ r, n) - 1;
+  }
+
+
+
+
 }
